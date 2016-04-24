bb75ff558ec62c7ce1bddf797acbd7575a54c48f
David Leigh <davidleigh1@gmail.com>
2016-04-24 15:52:07 +0300

    first commit

---
+++ template.js
@@ -0,0 +1,17 @@
+if (Meteor.isClient) {
+
+// Template.myTemplateName.helpers
+
+
+// Template.myTemplateName.events
+
+
+};
+
+if (Meteor.isServer) {
+	// None currently defined
+};
+
+Meteor.methods({
+	// None currently defined
+});
